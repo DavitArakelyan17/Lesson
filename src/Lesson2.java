f8f405a16fac83782e4f08e81390eeba12dc9b70
DavitArakelyan17 <dav4work@gmail.com>
2026-07-01 09:32:11 -0700

    First lesson

---
+++ src/Lesson2.java
@@ -0,0 +1,6 @@
+public class Lesson2 {
+    public static void main(String[] args){ 
+        int age = 10_000;
+        System.out.println(age);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) {
+        char frame = 43;
+        String Title = frame + "-----------------------Resume----------------------" + frame;
+        System.out.println(Title);
+        String FullName = "| FullName        -- Davit Arakelyan --             |";
+        System.out.println(FullName);
+        char border = '|';
+        String DateOfBirth = border +  " DateOfBirth        --17/09/2004 --                " + border;
+        System.out.println(DateOfBirth);
+        byte AgeNumber = 21;
+        String Age = border + " Age                   -- " + AgeNumber + " --                    " + border;
+        System.out.println(Age);
+        String Education = "| Education     -- Bachelor's Degree --             |";
+        System.out.println(Education);
+        String University = "| University           -- UFAR --                   |";
+        System.out.println(University);
+        String Faculty  = "| Faculty -- Informatics and Applied Mathematics -- |";
+        System.out.println(Faculty);
+        String Border = frame + "---------------------------------------------------" + frame;
+        System.out.println(Border);
+
+
+    }
+}
